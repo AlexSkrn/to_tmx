d8ff262d210bbb63b1586331439e78c47ff38282
alexskrn <askrn123@gmail.com>
2020-08-05 16:31:06 +0300

    Changed version number

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ except FileNotFoundError:
 
 setuptools.setup(
     name=NAME,
-    version='1.0.0',
+    version='1.0.1',
     description=DESCRIPTION,
     long_description=long_description,
     long_description_content_type='text/markdown',
